7aa98908df7117b21b85ce50e3999ede29ae01a2
Immanuel Ben Hefer <immanuel.ben.hefer@sap.com>
2021-05-14 16:16:51 +0300

    create scraper

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from elastic.v7.Elastic import main as elastic_main
 from hcva.parser.parser import main as parser_main
 from hcva.extra.sync import uploadSync, downloadSync
-from hcva.scrapers.SupremeCourt_Scraper import main as scraper_main
+from hcva.scraper.scrapers.SupremeCourt_Scraper import main as scraper_main
 
 if __name__ == '__main__':
     functions = {1: scraper_main,
