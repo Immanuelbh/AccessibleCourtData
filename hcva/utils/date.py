eb46f76116109257302a20adcf534379806f5804
Immanuel Ben Hefer <immanuel.ben.hefer@sap.com>
2021-06-05 22:00:17 +0300

    lint date

--- hcva/utils/date.py
+++ hcva/utils/date.py
@@ -1,11 +1,11 @@
-import pandas as pd
 from datetime import timedelta, date, datetime
+import pandas as pd
 
 SATURDAY = 6  # according to weekdays by datetime (monday=0, ...)
 
 
-def get_day_before(d):
-    prev = d - timedelta(1)
+def get_day_before(date_):
+    prev = date_ - timedelta(1)
     if prev.weekday() == SATURDAY:
         prev = prev - timedelta(1)
 
@@ -18,9 +18,9 @@ def get_yesterday():
 
 def init_dates(date_start='1-1-1997', date_end=get_yesterday()):
     ds = date_start.split('-')
-    es = date_end.split('-')
+    de = date_end.split('-')
     s = date(int(ds[2]), int(ds[1]), int(ds[0]))
-    e = date(int(es[2]), int(es[1]), int(es[0]))-timedelta(1)
+    e = date(int(de[2]), int(de[1]), int(de[0]))-timedelta(1)
     weekmask = "Sun Mon Tue Wed Thu Fri"
     dates = [d.strftime('%d-%m-%Y')
              for d in pd.bdate_range(start=s, end=e, freq='C', weekmask=weekmask)]
