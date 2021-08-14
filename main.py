2a6c764580cd1627f87fdc30ad179227207e5e5f
Immanuel Ben Hefer <immanuel.ben.hefer@sap.com>
2021-08-14 20:08:00 +0300

    add enricher

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
 from hcva.elastic.v7.elastic import main as elastic
 from hcva.parser.parser import main as parser
+from hcva.parser.enricher.main import normalize
 from hcva.scraper.app import main as scraper
 
 if __name__ == '__main__':
     functions = {1: scraper,
                  2: parser,
-                 3: elastic,
+                 3: normalize,
+                 4: elastic,
                  }
 
     if len(sys.argv) > 1:
@@ -15,7 +17,8 @@ if __name__ == '__main__':
         choice = int(input("Enter which function you want to run:\n"
                            "1: Scraper\n"
                            "2: Parser\n"
-                           "3: Elastic\n"
+                           "3: Enricher\n"
+                           "4: Elastic\n"
                            ))
 
     if len(functions) >= choice > 0:
