1898e864820d73422a68b69a4cda364fd228ead3
Immanuel Ben Hefer <immanuel.ben.hefer@sap.com>
2021-09-02 19:49:06 +0300

    fix sleep function

--- hcva/utils/time.py
+++ hcva/utils/time.py
@@ -1,10 +1,10 @@
 from time import sleep
 
 
-def call_sleep(logger=None, days=0, hours=0, minutes=0, seconds=0):
-    massage = f"Going to sleep for {days} days, {hours} hours, {minutes} minutes, {seconds} seconds"
+def call_sleep(logger=None, minutes=0, seconds=0):
+    massage = f"Going to sleep for {minutes} minutes"
     if logger is not None:
         logger.info(massage)
     else:
         print(massage)
-    sleep((days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60) + seconds)
+    sleep((minutes * 60) + seconds)
