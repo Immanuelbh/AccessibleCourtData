c3a869b01722554e9fdc6a5c40b4c8dcfef4b76e
Immanuel Ben Hefer <immanuel.ben.hefer@sap.com>
2021-09-08 15:29:22 +0300

    update init_app.sh

--- init_app.sh
+++ init_app.sh
@@ -3,7 +3,7 @@
 ps -ef | grep main.py | grep -v grep | awk '{print $2}' | xargs kill
 ps -ef | grep opt/google/chrome/chrome | grep -v grep | awk '{print $2}' | xargs kill
 
-nohup python3 ./main.py 1 > ./logs/out/scraper.out &
-nohup python3 ./main.py 2 > ./logs/out/parser.out &
-nohup python3 ./main.py 3 > ./logs/out/enricher.out &
-nohup python3 ./main.py 4 > ./logs/out/elastic.out
+nohup python3 ./main.py 1 > ./logs/hcva/scraper.out &
+nohup python3 ./main.py 2 > ./logs/hcva/parser.out &
+nohup python3 ./main.py 3 > ./logs/hcva/enricher.out &
+nohup python3 ./main.py 4 > ./logs/hcva/elastic.out
