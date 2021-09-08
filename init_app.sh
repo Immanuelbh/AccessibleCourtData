8fe0707f712a1e9d778437864280ad9547a27d30
Immanuel Ben Hefer <immanuel.ben.hefer@sap.com>
2021-09-08 15:42:39 +0300

    update init_app.sh

--- init_app.sh
+++ init_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ps -ef | grep main.py | grep -v grep | awk '{print $2}' | xargs kill
-ps -ef | grep opt/google/chrome/chrome | grep -v grep | awk '{print $2}' | xargs kill
+ps ax | grep main.py | grep -v grep | awk '{print $1}' | xargs kill
+ps ax | grep /usr/lib/firefox/firefox | grep -v grep | awk '{print $1}' | xargs kill
 
 nohup python3 ./main.py 1 > ./logs/hcva/scraper.out &
 nohup python3 ./main.py 2 > ./logs/hcva/parser.out &
