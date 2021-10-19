e0dce5f5402a9de2960975a3297e32742e653d16
Immanuel Ben Hefer <immanuel.ben.hefer@sap.com>
2021-10-19 21:18:22 +0300

    add err logs

--- init_app.sh
+++ init_app.sh
@@ -3,7 +3,7 @@
 ps ax | grep main.py | grep -v grep | awk '{print $1}' | xargs kill
 ps ax | grep /usr/lib/firefox/firefox | grep -v grep | awk '{print $1}' | xargs kill
 
-nohup python3 ./main.py 1 > ./logs/hcva/scraper.out &
-nohup python3 ./main.py 2 > ./logs/hcva/parser.out &
-nohup python3 ./main.py 3 > ./logs/hcva/enricher.out &
-nohup python3 ./main.py 4 > ./logs/hcva/elastic.out
+nohup python3 ./main.py 1 > ./logs/hcva/scraper.out 2> ./logs/hcva/scraper.err.out < /dev/null &
+nohup python3 ./main.py 2 > ./logs/hcva/parser.out 2> ./logs/hcva/parser.err.out < /dev/null &
+nohup python3 ./main.py 3 > ./logs/hcva/enricher.out 2> ./logs/hcva/enricher.err.out < /dev/null &
+nohup python3 ./main.py 4 > ./logs/hcva/elastic.out 2> ./logs/hcva/elastic.err.out < /dev/null &
