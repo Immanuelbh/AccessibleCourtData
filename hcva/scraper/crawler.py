e21b05d0e2655e26a49d9470296c18e8be5af0aa
Immanuel <37409984+Immanuelbh@users.noreply.github.com>
2021-11-14 20:00:21 +0200

    update crawler error messages (#57)
    * change log to error type
    
    * add error messages

--- hcva/scraper/crawler.py
+++ hcva/scraper/crawler.py
@@ -79,53 +79,46 @@ class Crawler:
     # do - close web driver
     def close(self):
         self._driver.quit()  # close the browser
-        message = 'Closing browser'
-        self._logger.info(message)
+        self._logger.info('Closing browser')
         return True
 
     # output - return True if successful
     # do - return to previous page
     def go_back(self):
         self._driver.back()
-        message = 'went to previous page'
-        self._logger.info(message)
+        self._logger.info('went to previous page')
         return True
 
     # output - return True if successful
     # do - refresh page loaded on crawler
     def refresh(self):
         self._driver.refresh()
-        message = 'Refresh page'
-        self._logger.info(message)
+        self._logger.info('Refresh page')
         return True
 
     # input - frame as web element
     # do
     def switch_frame(self, frame):
         self._driver.switch_to.frame(frame)
-        message = 'switch to frame'
-        self._logger.info(message)
+        self._logger.info('switch to frame')
         return True
 
     # do - switch windows handle after case was clicked
     def switch_window_handle(self, index=0):
         window = self._driver.window_handles[index]
         self._driver.switch_to.window(window)
-        message = 'switch window handle'
-        self._logger.info(message)
+        self._logger.info('switch window handle')
         return True
 
     # do - switch to default content
     def switch_to_default_content(self):
         self._driver.switch_to.default_content()
-        message = 'switch to default content'
-        self._logger.info(message)
+        self._logger.info('switch to default content')
         return True
 
     def get_page_source(self):
         page_source = self._driver.page_source
-        message = 'Got page Source'
-        self._logger.info(message)
+        self._logger.info('Got page Source')
         return page_source
 
     # input - elem_type as string, string as string
@@ -193,22 +186,19 @@ class Crawler:
             self._logger.info(message)
             return elem
 
-        except TimeoutException as _:  # did not found elem in time
+        except TimeoutException as te:  # did not found elem in time
             if raise_error:
-                message = f'Did not find elem: {string}, type: {elem_type}, delay: {delay} in time'
-                self._logger.exception(message)
+                self._logger.error(f'Did not find elem: {string}, type: {elem_type}, delay: {delay} in time', te)
             return None
 
-        except ElementNotVisibleException as _:  # did not found elem
+        except ElementNotVisibleException as enve:  # did not found elem
             if raise_error:
-                message = f'Elem is not visible: {string}, type: {elem_type}'
-                self._logger.exception(message)
+                self._logger.error(f'Elem is not visible: {string}, type: {elem_type}', enve)
             return None
 
-        except NoSuchElementException as _:  # did not found elem
+        except NoSuchElementException as nse:  # did not found elem
             if raise_error:
-                message = f'No Such elem: {string}, type: {elem_type}'
-                self._logger.exception(message)
+                self._logger.error(f'No Such elem: {string}, type: {elem_type}', nse)
             return None
         finally:
             if raise_error is False:
@@ -221,13 +211,11 @@ class Crawler:
         try:
             hover = ActionChains(driver).move_to_element(elem)
             hover.perform()
-            message = 'elem got hovered'
-            self._logger.info(message)
+            self._logger.info('elem got hovered')
             return True
 
-        except Exception as _:
-            message = 'Could not hover that'
-            self._logger.exception(message)
+        except Exception as e:
+            self._logger.error('Could not hover that', e)
             return False
 
     # input - elem as web element, value as string, msg as string
@@ -244,10 +232,8 @@ class Crawler:
             self._logger.info(message)
             return True
 
-        except ElementNotSelectableException as _:
-            if message is None:
-                message = 'Could not select that'
-            self._logger.error(message)
+        except ElementNotSelectableException as e:
+            self._logger.error('error selecting element', e)
             return False
 
     # input - elem as web element, msg as string
@@ -262,14 +248,12 @@ class Crawler:
             self._logger.info(message)
             return True
 
-        except ElementClickInterceptedException as _:
-            message = 'Element Click Intercepted'
-            self._logger.exception(message)
+        except ElementClickInterceptedException as ecie:
+            self._logger.error('Element Click Intercepted', ecie)
             return False
 
-        except ElementNotInteractableException as _:
-            message = 'Element Not Interactable'
-            self._logger.exception(message)
+        except ElementNotInteractableException as enie:
+            self._logger.error('Element Not Interactable', enie)
             return False
 
     # input - elem as web element, data as string
@@ -286,9 +270,8 @@ class Crawler:
             self._logger.info(message)
             return True
 
-        except Exception as _:
-            message = 'Could not send elem this data'
-            self._logger.exception(message)
+        except Exception as e:
+            self._logger.error('Could not send elem this data', e)
             return False
 
     # input - elem as web element
@@ -298,14 +281,12 @@ class Crawler:
         try:
             text = elem.text  # get elem text
             self._text_query = text
-            message = f'Got text from elem: {text}'
-            self._logger.info(message)
+            self._logger.info(f'Got text from elem: {text}')
             return True
 
-        except Exception as _:
+        except AttributeError as e:
             self._text_query = None
-            message = 'Could not get elem text'
-            self._logger.exception(message)
+            self._logger.error('Could not get elem text, error: ', e)
             return False
 
     # input - driver as web driver, N is the index we want to reach as int
@@ -323,13 +304,11 @@ class Crawler:
     def exec_script(self, driver, elem, string):
         try:
             driver.execute_script(string, elem)
-            message = 'Script executed'
-            self._logger.info(message)
+            self._logger.info('Script executed')
             return True
 
-        except JavascriptException as _:
-            message = 'Could not execute script'
-            self._logger.exception(message)
+        except JavascriptException as je:
+            self._logger.error('Could not execute script', je)
             return False
 
     # input - result as string
@@ -350,11 +329,9 @@ class Crawler:
             driver.switch_to.default_content()  # return to main window
 
             self._text_query = text
-            message = f'Alert say: {text}'
-            self._logger.info(message)
+            self._logger.info(f'Alert say: {text}')
             return True
 
-        except NoAlertPresentException as _:
-            message = 'Did not find any alert'
-            self._logger.error(message)
+        except NoAlertPresentException as nape:
+            self._logger.error('Did not find any alert', nape)
             return False
